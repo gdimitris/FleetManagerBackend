c2e092fff3f0e291c3a1889cc70eed856cc08e17
gdimitris <dimitrios.yiannakos@gmail.com>
2015-11-17 19:26:47 +0200

    Fixed polyline and infowindow on markers

--- Application/views.py
+++ Application/views.py
@@ -8,13 +8,16 @@ from models import LocationPoints
 
 @app.route('/', methods=['GET', 'POST'])
 def root():
-    return render_template('index.html')
+    result_list = get_distinct_phone_ids_from_db()
+    return render_template('users.html', phone_ids=result_list)
+
 
 @app.route('/<device_id>', methods=['GET'])
 def show_device_locations(device_id):
     locations = get_location_points_with_id(device_id)
     ser_locations = [l.serialize for l in locations]
     return render_template('index.html', location_points=json.dumps(ser_locations))
+
 
 @app.route('/<device_id>', methods=['POST'])
 def add_entry(device_id):
@@ -30,6 +33,15 @@ def insert_location_point_in_db(device_id, latitude, longitude, timestamp):
     lp = LocationPoints(phone_id=device_id, latitude=latitude, longitude=longitude, timestamp=timestamp)
     db.session.add(lp)
     db.session.commit()
+    db.session.flush()
+
+
+def get_distinct_phone_ids_from_db():
+    res = db.session.query(LocationPoints).distinct(LocationPoints.phone_id).group_by(LocationPoints.phone_id)
+    result_list = list()
+    for val in res:
+        result_list.append(val.phone_id)
+    return result_list
 
 
 def get_location_points_with_id(phone_id):
